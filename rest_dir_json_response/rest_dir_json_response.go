0aa75cf3050df4c364d0fc5ca52e2f0a21dcf281
Chris Lam <chris.lam@oracle.com>
2015-11-17 15:25:16 -0600

    first attempt at making thread safe

--- rest_dir_json_response/rest_dir_json_response.go
+++ rest_dir_json_response/rest_dir_json_response.go
@@ -10,29 +10,36 @@ import (
 	"path/filepath"
 )
 
-//not thread safe
-var list = make([]string, 0)
-
 func init() {
 	fmt.Println("initialize block")
 }
 
-func visit(path string, f os.FileInfo, err error) error {
+func getJobNames(dir string, writer http.ResponseWriter) {
+	list := make([]string, 0)
+	err := filepath.Walk(dir, func(dir string, f os.FileInfo, err error) error {
+		matched, err := filepath.Match("*.sh", f.Name())
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if matched {
+			list = append(list, dir)
+		}
+
+		return nil
+	})
+
+	jobNames, err := json.Marshal(list)
+
 	if err != nil {
-		fmt.Println(err)
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	matched, err := filepath.Match("*.sh", f.Name())
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(jobNames)
 
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if matched {
-		list = append(list, path)
-	}
-
-	return nil
 }
 
 func main() {
@@ -40,16 +47,8 @@ func main() {
 	router.GET("/jobnames", func(writer http.ResponseWriter, router *http.Request, _ httprouter.Params) {
 		flag.Parse()
 		dirRoot := flag.Arg(0)
-		err := filepath.Walk(dirRoot, visit)
-		jobNames, err := json.Marshal(list)
 
-		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		writer.Header().Set("Content-Type", "application/json")
-		writer.Write(jobNames)
+		getJobNames(dirRoot, writer)
 	})
 
 	http.ListenAndServe(":8990", router)
